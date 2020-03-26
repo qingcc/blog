3d0423ce28fd41cf6b526df65c638f7e066bb20f
qing <qingcc0503@163.com>
2020-03-26 19:13:34 +0800

    update docker-compose.yml

--- hexo.sh
+++ hexo.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 container_name=hexo_blog
-docker run -p 7000:4000 -dit --name $container_name \
-    -v ~/.ssh:/root/.ssh \
-    -v /root/docker/hexo/source:/blog/source \
-    -v /root/docker/hexo/themes:/blog/themes \
-    -v /root/docker/hexo/scaffolds:/blog/scaffolds \
-    -v /root/docker/hexo/_config.yml:/blog/_config.yml \
-qingcc/hexo_blog:latest
+docker-compose up -d
 
-docker exec $container_name hexo g 
+docker exec $container_name hexo c
+docker exec $container_name hexo g
 docker exec $container_name hexo s
     #你的用户文件夹路径
     #你的用户文件夹路径
