180b7638ef6772a8eb1e7428f0cf784828ec0288
qing <qingcc0503@163.com>
2020-04-27 10:57:55 +0800

    update start sh

--- hexo.sh
+++ hexo.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 container_name=hexo_blog
 
+exist=`docker ps -a | $container_name`
+
+if [ "${exist}" != "" ]; then
+  docker rm -f $container_name
+fi
+
 while (( $# > 1 )); do case $1 in
    -c) container_name="$2";;
    *) break;
